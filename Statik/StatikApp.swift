a0b66289d75ada8d1d3cb4b1fb97e956554deb46
Luca <paesberaldiluca@gmail.com>
2025-02-28 09:27:11 -0300

    Initial Commit

---
+++ Statik/StatikApp.swift
@@ -0,0 +1,17 @@
+//
+//  StatikApp.swift
+//  Statik
+//
+//  Created by Luca on 28/02/25.
+//
+
+import SwiftUI
+
+@main
+struct StatikApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ StatikTests/StatikTests.swift
@@ -0,0 +1,17 @@
+//
+//  StatikTests.swift
+//  StatikTests
+//
+//  Created by Luca on 28/02/25.
+//
+
+import Testing
+@testable import Statik
+
+struct StatikTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
