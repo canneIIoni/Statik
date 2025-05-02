a8bb100ae8a4e40109111bd1e202fede919e0585
Luca <paesberaldiluca@gmail.com>
2025-05-01 21:24:55 -0300

    frontend finetuning

--- Statik/Components/SongComponentView.swift
+++ Statik/Components/SongComponentView.swift
@@ -15,29 +15,82 @@ struct SongComponentView: View {
     var body: some View {
         HStack {
             VStack(alignment: .leading) {
-                HStack(alignment: .bottom) {
-                    Text("\(song.title) ·")
-                        .font(.system(size: 20))
-                        .multilineTextAlignment(.leading)
-                    RatingView(
-                        rating: Binding(
-                            get: { song.grade },
-                            set: { song.grade = $0 }
-                        ),
-                        starSize: $smallStarSize,
-                        editable: .constant(false)
-                    ).allowsHitTesting(false)
-                        .padding(.bottom, 3.2)
+                if song.title.count < 20 {
+                    HStack(alignment: .bottom) {
+                        Text("\(song.title) ")
+                            .font(.system(size: 20))
+                            .multilineTextAlignment(.leading)
+                        RatingView(
+                            rating: Binding(
+                                get: { song.grade },
+                                set: { song.grade = $0 }
+                            ),
+                            starSize: $smallStarSize,
+                            editable: .constant(false)
+                        ).allowsHitTesting(false)
+                            .padding(.bottom, 3.2)
+                    }
+                } else {
+                    if song.title.count < 36 {
+                        Text("\(song.title) ")
+                            .font(.system(size: 20))
+                            .multilineTextAlignment(.leading)
+                            .frame(height: 10)
+                    } else {
+                        Text("\(song.title) ")
+                            .font(.system(size: 20))
+                            .multilineTextAlignment(.leading)
+                    }
+                    
+                    if song.title.count < 36 {
+                        RatingView(
+                            rating: Binding(
+                                get: { song.grade },
+                                set: { song.grade = $0 }
+                            ),
+                            starSize: $smallStarSize,
+                            editable: .constant(false)
+                        ).allowsHitTesting(false)
+                            .padding(.bottom, 3.2)
+                    } else {
+                        RatingView(
+                            rating: Binding(
+                                get: { song.grade },
+                                set: { song.grade = $0 }
+                            ),
+                            starSize: $smallStarSize,
+                            editable: .constant(false)
+                        ).allowsHitTesting(false)
+                            .offset(y: -5)
+                            .padding(.bottom, 3.2)
+                    }
+                    
                 }
-                Text("\(artist)")
-                    .font(.system(size: 14))
-                    .foregroundColor(.gray)
-                    .multilineTextAlignment(.leading)
-                if !song.review.isEmpty {
-                    Text("\(song.review)")
+                
+                if song.title.count >= 20 && song.title.count < 36 {
+                    Text("\(artist)")
                         .font(.system(size: 14))
-                        .foregroundColor(.secondaryText)
+                        .foregroundColor(.gray)
                         .multilineTextAlignment(.leading)
+                    if !song.review.isEmpty {
+                        Text("\(song.review)")
+                            .font(.system(size: 14))
+                            .foregroundColor(.secondaryText)
+                            .multilineTextAlignment(.leading)
+                    }
+                } else {
+                    Text("\(artist)")
+                        .font(.system(size: 14))
+                        .foregroundColor(.gray)
+                        .multilineTextAlignment(.leading)
+                        .offset(y: -5)
+                    if !song.review.isEmpty {
+                        Text("\(song.review)")
+                            .font(.system(size: 14))
+                            .foregroundColor(.secondaryText)
+                            .multilineTextAlignment(.leading)
+                            .offset(y: -5)
+                    }
                 }
             }
             Spacer()
